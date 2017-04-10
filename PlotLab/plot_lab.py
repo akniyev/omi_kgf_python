f8d837ad1be087e6268c0a5528eae13aa82b91f7
Hasan A <hasan.akniyev@gmail.com>
2017-04-10 17:47:51 +0300

    Added some code

--- PlotLab/plot_lab.py
+++ PlotLab/plot_lab.py
@@ -1,45 +1,123 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
 # Model
 class Node:
     def __init__(self):
         self.id = None
-        self.input_variables = {}
-        self.output_variables = {}
+        self.input_connections : List[Connection] = []         # [Connection]
+        self.output_connections : List[Connection] = []        # [Connection]
 
-        self.function_code = ""
-        self.display_function_code = ""
-        self.import_modules = []  # list of include strings
+        self.function : Function = None                # Function
+        self.display_function : Function = None        # Function
+        self.import_modules : List[str] = []            # [String], example: ['import numpy as np']
+
+    def __init__(self, input_connection_names : List[str], output_connection_names : List[str]):
+        self.__init__()
+
+        self.input_connections = Connection.generate_connection_list(input_connection_names)
+        self.output_connections = Connection.generate_connection_list(output_connection_names)
+
+    def clear_input_connections(self):
+        self.input_connections = []
+
+    @staticmethod
+    def find_connection(connections, connection_name):
+        for i in range(len(connections)):
+            if connections[i].name == connection_name:
+                return connections[i]
+        return False
+
+    @staticmethod
+    def delete_connection(self, connections : List[Connection], connection_name : str):
+        c = self.find_connection(connections, connection_name)
+        if c:
+            connections.remove(c)
+
+    def delete_input_connection(self, connection_name : str):
+        self.delete_connection(self.input_connections, connection_name)
+
+    def delete_output_connection(self, connection_name : str):
+        self.delete_connection(self.output_connections, connection_name)
+
+    @staticmethod
+    def add_connection(self, connections : List[Connection], connection_name : str):
+        c = self.find_connection(connections, connection_name)
+        if not c:
+            c = Connection()
+            c.name = connection_name
+            connections.append(c)
+        else:
+            print('Connection with this name already exists!')
+
+    def add_input_connection(self, connection_name : str):
+        self.add_connection(self.input_connections, connection_name)
+
+    def add_output_connections(self, connection_name):
+        self.add_connection(self.output_connections, connection_name)
+
+    def connection_exists(self, connections : List[Connection], connection_name : str):
+        return not self.find_connection(connections, connection_name)
+
+    def input_connection_exists(self, connection_name : str):
+        return self.connection_exists(self.input_connections, connection_name)
+
+    def output_connection_exists(self, connection_name : str):
+        return self.connection_exists(self.output_connections, connection_name)
 
 
-class Link:
+class Function(ABC):
+    @abstractmethod
+    def compute(self, input_connections, output_connections):
+        pass
+
+
+class Connection:
     def __init__(self):
-        self.start_node_id = None
-        self.end_node_id = None
-        self.start_output_name = None
-        self.end_input_name = None
+        self.name = None
+        self.value = None
+        self.target_node = None
+        self.target_connection = None
+
+    @staticmethod
+    def generate_connection_list(names : List[str]):
+        result = []
+        for name in names:
+            connection = Connection()
+            connection.name = name
+            result.append(connection)
+
+        return result
+
+
+class Coordinate:
+    x = 0
+    y = 0
 
 
 class Plot:
     def __init__(self):
-        self.nodes = []
-        self.links = []
-        self.nodes_coordinates = {}  # {id : (x, y)}
+        self.nodes = {}
+        self._id = 0
 
-    def add_node(self, node, id):
-        node.id = id
-        self.nodes.append(node)
-        return True
+    def next_id(self):
+        self._id += 1
+        return self._id
 
-    def add_link(self, start_node_id, start_output_name, end_node_id, end_input_name):
-        # check: in, out nodes exist
-        # check: in, out nodes have such values
-        # check: out_value1 and in_value2 not occupied
-        link = Link()
-        link.start_node_id = start_node_id
-        link.end_node_id = end_node_id
-        link.start_output_name = start_output_name
-        link.end_input_name = end_input_name
-        self.links.append(link)
-        return True
+    def add_node(self, node: Node):
+        node_id = self.next_id()
+        node.id = node_id
+        self.nodes[node_id] = node
+
+    def remove_node_by_id(self, node_id):
+        del self.nodes[node_id]
+
+    def connect_nodes(self, ):
+
+
+
+
 
 
 
