9cc244ff27ab8b965dd28ea2276cd46de14b180b
Hasan A <hasan.akniyev@gmail.com>
2017-04-21 18:28:58 +0300

    Some more tests

--- learning/qml/qml_test.py
+++ learning/qml/qml_test.py
@@ -1,11 +1,27 @@
 import sys
-from PyQt5.QtCore import Qt, QUrl
-from PyQt5.QtQuick import QQuickView
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
+from PyQt5.QtCore import Qt, QUrl, QObject
+from PyQt5.QtGui import QPainter
+from PyQt5.QtQml import qmlRegisterType
+from PyQt5.QtQuick import QQuickView, QQuickItem, QQuickPaintedItem
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QPushButton
 
 
 def on_resize(sender):
     print("resize")
+
+name_field: QQuickItem = None
+
+def save_button_clicked(name):
+    print('mouse clicked %s' % name)
+
+
+class MyPaintedItem(QQuickPaintedItem):
+    def __init__(self, parent: QQuickItem):
+        super().__init__(parent)
+
+    def paint(self, painter: QPainter):
+        painter.drawEllipse(10, 10, 20, 20)
+
 
 # Main Function
 if __name__ == '__main__':
@@ -13,10 +29,18 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     # Create a label and set its properties
 
+    qmlRegisterType(MyPaintedItem, 'MyPaintedItem', 1, 0, 'MyPaintedItem')
+
     w_settings = QQuickView()
     w_settings.setSource(QUrl('settings_window.qml'))
-
+    w_settings.setResizeMode(QQuickView.SizeRootObjectToView)
     w_settings.show()
 
+    # button_save: QQuickItem = w_settings.rootObject().findChild(QQuickItem, 'button_save')
+    # button_reset: QQuickItem = w_settings.rootObject().findChild(QQuickItem, 'button_reset')
+    # name_field = w_settings.rootObject().findChild(QQuickItem, 'name_field')
+    # name_field.setProperty('text', 'Hello!')
+
+    # button_save.pressed.connect(save_button_clicked)
 
     sys.exit(app.exec_())
--- learning/qml/settings_window.qml
+++ learning/qml/settings_window.qml
@@ -1,9 +1,20 @@
 import QtQuick 2.0
 import QtQuick.Controls 1.4
 import QtQuick.Layouts 1.3
+import QtQuick.Dialogs 1.1
+import MyPaintedItem 1.1
 
 Rectangle {
+    id: mainRect
+    objectName: "main_rect"
     width: 300; height: 600
+
+    MessageDialog {
+            id: msg
+            title: "Title"
+            text: "Button pressed"
+            onAccepted: visible = false
+        }
 
     ColumnLayout {
         id: columnLayout
@@ -13,6 +24,11 @@ Rectangle {
         anchors.topMargin: 5
         anchors.fill: parent
 
+        MyPaintedItem {
+            id: paintedItem
+            objectName: "paintedItem"
+        }
+
         Label {
             id: lb_name
             text: qsTr("Name")
@@ -21,6 +37,7 @@ Rectangle {
 
         TextField {
             id: tf_name
+            objectName: "name_field"
             x: 8
             y: 232
             Layout.fillWidth: true
@@ -79,13 +96,20 @@ Rectangle {
 
             Button {
                 id: b_save
+                objectName: "button_save"
+
                 text: qsTr("Save")
                 Layout.fillHeight: true
                 Layout.fillWidth: true
+                signal pressed(string name)
+                onClicked: {
+                    this.pressed(this.objectName)
+                }
             }
 
             Button {
                 id: b_reset
+                objectName: "button_reset"
                 text: qsTr("Reset")
                 Layout.fillHeight: true
                 Layout.fillWidth: true
